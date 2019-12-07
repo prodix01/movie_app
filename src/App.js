dbe3df5d29d101f2c2af8693e7276564fd1b8bab
kim <s158614@nishitech.ac.jp>
2019-12-07 20:57:39 +0900

    All you need to know about State

--- src/App.js
+++ src/App.js
@@ -1,57 +1,43 @@
-import React from 'react';
-import Coffee from "./Coffee";
+
+import React, {Component} from "react";
 
 
 
 
-const foodILike = [
-    {
-        id : 1,
-        name: "Kimchi",
-        image:
-            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-        rating : 3.2
-    },
-    {
-        id : 2,
-        name: "Samgyeopsal",
-        image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-        rating : 3.8
-    },
-    {
-        id : 3,
-        name: "Bibimbap",
-        image:
-            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-        rating : 4.5
-    },
-    {
-        id : 4,
-        name: "Doncasu",
-        image:
-            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-        rating : 1.4
-    },
-    {
-        id : 5,
-        name: "Kimbap",
-        image:
-            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-        rating : 3.0
+
+
+class App extends Component {
+    // 상태값
+
+    state = {
+        count : 0
+    };
+
+
+
+
+
+    // 함수
+
+    add = () => {
+      this.setState(current => ({count : current.count + 1}));
+    };
+
+    minus = () => {
+      this.setState(current =>({count : current.count - 1 }));
+    };
+
+    // 라이프사이클
+    render() {
+        const {count} = this.state;
+        return (
+            <div>
+                <h1>State is : {count}</h1>
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+            </div>
+        );
     }
-];
-
-
-
-function App() {
-  return (
-    <div>
-        {foodILike.map(dish => (
-            <Coffee key={dish.id} favorite={dish.image} name={dish.name} rating={dish.rating}/>
-        ))}
-    </div>
-  );
-}
+};
 
 export default App;
