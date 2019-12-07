0c97f20ea68c8e24d0cf5d9f38712714d39c4cb3
kim <s158614@nishitech.ac.jp>
2019-12-07 20:36:06 +0900

    Add propTypes

--- src/Coffee.js
+++ src/Coffee.js
@@ -1,13 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Coffee({ favorite, name }) {
+
+function Coffee({ favorite, name, rating }) {
     return (
         <div>
             <h1>I like {name}</h1>
+            <h4>{rating} / 5.0</h4>
             <img src={favorite} alt={name} />
         </div>
     );
 
 }
+
+
+Coffee.propTypes = {
+    name : PropTypes.string.isRequired,
+    favorite : PropTypes.string.isRequired,
+    rating : PropTypes.number.isRequired
+};
+
+
 
 export default Coffee;
